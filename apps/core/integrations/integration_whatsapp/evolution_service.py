d90ce6746088c5e4751e18bcbc48d575b7f1515e
Bruno Vidal <brunovidal27.19@gmail.com>
2025-12-19 16:46:15 -0300

    feat: Reestruturar integração com a Evolution API, removendo código obsoleto e organizando a estrutura de arquivos para melhor manutenção

---
+++ apps/core/integrations/integration_whatsapp/evolution_service.py
@@ -0,0 +1,22 @@
+# evolution_service.py
+import logging
+from typing import List
+from integrations.integration_whatsapp.whatsapp import get_evolution_client
+from evolutionapi.models.message import TextMessage
+
+# Logger
+logger = logging.getLogger("integrations")
+
+# Singleton global do client
+CLIENT = get_evolution_client()
+
+# Busca instâncias apenas uma vez
+if CLIENT:
+    INSTANCES = CLIENT.instances.fetch_instances()
+else:
+    INSTANCES = []
+
+CONTEXT = {"instances": INSTANCES}
+
+
+
